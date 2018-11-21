8d7e723ec1c171566a91920649a8c89373f7cce8
Anthony Kalaitzis <anthony.kalaitzis@gmail.com>
2018-11-21 18:52:28 +1030

    Hopefully works between computers now

--- init.el
+++ init.el
@@ -1,18 +1,25 @@
+;; Init.el file, initializes emacs
+
 ;; (server-start)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
 (eval-when-compile
   (require 'use-package))
+
 (let ((file-name-handler-alist nil)) 
-  ;; Put custom settings in separate file.
+  ;; Load custom File
   (setq custom-file "~/.emacs.d/custom.el")
   (load custom-file :noerror)
   
-  ;; don't ask user to confirm emacs-lisp execution
+  ;; Load org file
   (defun my-org-confirm-babel-evaluate (lang body)
     (not (string= lang "emacs-lisp")))  
   (setq org-confirm-babel-evaluate 'my-org-confirm-babel-evaluate)
   (org-babel-load-file "~/.emacs.d/config.org")
+
+  ;; EL file version
+  (load-file "~/.emacs.d/config.el")
 )
