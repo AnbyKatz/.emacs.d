6bc64c7a8b12f70931dfcb2930666f367743e618
Anthony Kalaitzis <anthony.kalaitzis@adelaide.edu.au>
2019-02-06 08:40:01 +1030

    changed new computer file to shell

---
+++ NewComputerSetup.sh
@@ -0,0 +1,28 @@
+# Anthony Kalaitzis
+# New Computer Setup file
+
+# ---------------------------Basic Libraries--------------------------
+sudo apt-get install texlive texlive-latex-extra gfortran g++
+
+
+# ---------------------------Emacs Libraries--------------------------
+sudo apt-get install build-essential texinfo libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev libxpm-dev automake autoconf
+# Type the following below the install the package
+apt-cache search 'libgnutls.*-dev'
+# Download source from [[https://www.gnu.org/software/emacs/][Emacs website]]
+
+
+# -------------------------------Python-------------------------------
+[[https://www.anaconda.com/download/#linux][Anaconda download page]]
+
+
+# -------------------------------Fortran------------------------------
+Download from below, username: anthony42
+[[https://software.intel.com/en-us/qualify-for-free-software/student][ifort compiler]]
+
+
+# ---------------------------Package Manager--------------------------
+Dropbox
+Spotify
+Steam
+Discord
