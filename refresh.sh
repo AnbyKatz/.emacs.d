b43a24c15e3b73eed85492a63eac8f81b25db0d6
Anthony Kalaitzis <anthony.kalaitzis@adelaide.edu.au>
2019-03-05 09:34:08 +1030

    added titles to refresh command

--- refresh.sh
+++ refresh.sh
@@ -2,7 +2,15 @@
 
 # ----------------------Updates all repositories----------------------
 
+echo '.emacs.d'
 git -C ~/.emacs.d/ status
+echo
+echo 'QMC'
 git -C ~/Documents/QMC status
+echo
+echo 'NumFort'
 git -C ~/Documents/NumFort status
+echo
+echo 'Thesis'
 git -C ~/Documents/Thesis status
+echo
