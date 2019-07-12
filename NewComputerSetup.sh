97812f112f5f79539036dad1297caa431f282c4a
Anthony Kalaitzis <anthony@pop-os.localdomain>
2019-07-12 23:23:54 +0930

    updated new install script

--- NewComputerSetup.sh
+++ NewComputerSetup.sh
@@ -1,21 +1,33 @@
 # Anthony Kalaitzis
 # New Computer Setup file
 
+# -----------------------------Everything-----------------------------
+sudo apt-get install texlive-full gfortran g++ gcc build-essential texinfo libx11-dev \
+     libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev \
+     libxpm-dev automake autoconf libgnutls28-dev python3-matplotlib python-numpy python-scipy \
+     python-matplotlib ipython3 python-pandas python-sympy python-nose
+
+
+
+# ----------------------------Individually----------------------------
 # ---------------------------Basic Libraries--------------------------
-sudo apt-get install texlive-full gfortran g++
+sudo apt-get install texlive-full gfortran g++ gcc
 
 # ---------------------------Emacs Libraries--------------------------
-sudo apt-get install build-essential texinfo libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev libxpm-dev automake autoconf
-# Type the following below then install the package
-apt-cache search 'libgnutls.*-dev'
-# Download source from [[https://www.gnu.org/software/emacs/][Emacs website]]
+sudo apt-get install build-essential texinfo libx11-dev libxpm-dev libjpeg-dev libpng-dev \
+     libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev libxpm-dev automake autoconf libgnutls28-dev
+# apt-cache search 'libgnutls.*-dev'
 
 # -------------------------------Python-------------------------------
-[[https://www.anaconda.com/download/#linux][Anaconda download page]]
+sudo apt-get install python3-matplotlib python-numpy python-scipy python-matplotlib \
+     ipython3 python-pandas python-sympy python-nose
 
 # -------------------------------Fortran------------------------------
 Download from below, username: anthony42
 [[https://software.intel.com/en-us/qualify-for-free-software/student][ifort compiler]]
+
+# -------------------------------Matlab-------------------------------
+Install from mathworks website
 
 # ---------------------------Package Manager--------------------------
 Dropbox
