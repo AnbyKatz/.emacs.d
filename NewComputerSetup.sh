29767ee3921bf3f2164b5769653e28260c51d25f
Anthony Kalaitzis <anthony@pop-os.localdomain>
2019-07-12 23:47:22 +0930

    fixed to add just python3 stuff

--- NewComputerSetup.sh
+++ NewComputerSetup.sh
@@ -4,10 +4,8 @@
 # -----------------------------Everything-----------------------------
 sudo apt-get install texlive-full gfortran g++ gcc build-essential texinfo libx11-dev \
      libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev \
-     libxpm-dev automake autoconf libgnutls28-dev python3-matplotlib python-numpy python-scipy \
-     python-matplotlib ipython3 python-pandas python-sympy python-nose
-
-
+     libxpm-dev automake autoconf libgnutls28-dev python3-matplotlib \
+     python3-numpy python3-scipy ipython3 python3-pandas python3-sympy python3-nose
 
 # ----------------------------Individually----------------------------
 # ---------------------------Basic Libraries--------------------------
@@ -19,8 +17,8 @@ sudo apt-get install build-essential texinfo libx11-dev libxpm-dev libjpeg-dev l
 # apt-cache search 'libgnutls.*-dev'
 
 # -------------------------------Python-------------------------------
-sudo apt-get install python3-matplotlib python-numpy python-scipy python-matplotlib \
-     ipython3 python-pandas python-sympy python-nose
+sudo apt-get install python3-matplotlib python3-numpy python3-scipy \
+     ipython3 python3-pandas python3-sympy python3-nose
 
 # -------------------------------Fortran------------------------------
 Download from below, username: anthony42
